a12835770a2ec291842aef48815c4b44cd853f9d
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-11-05 15:26:12 +0300

    complete

--- src/main/java/com/javaacademy/polyclinic/Polyclinic.java
+++ src/main/java/com/javaacademy/polyclinic/Polyclinic.java
@@ -1,7 +1,6 @@
 package com.javaacademy.polyclinic;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
